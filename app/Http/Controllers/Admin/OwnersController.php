3ecb0346b121c0b2b2e57e92db2178833475923a
tomosu20 <tomosu.cre820@gmail.com>
2024-06-05 17:51:47 +0900

    carbon

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Owner;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OwnersController extends Controller
 {
@@ -20,7 +23,23 @@ class OwnersController extends Controller
      */
     public function index()
     {
-        dd('test owner list');
+        $date_now = Carbon::now();
+        $date_parse = Carbon::parse(now());
+
+        echo $date_now->year;
+        echo $date_parse;
+
+        $e_all = Owner::all();
+        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $q_first = DB::table('owners')->select('name')->first();
+
+        // $c_test = collect([
+        //     'name' => 'test',
+        // ]);
+
+        // dd($e_all, $q_get, $q_first, $c_test);
+
+        return view('admin.owners.index', compact('e_all', 'q_get'));
     }
 
     /**
