6bcc0c15c073eeb02f7fdecdca130fe01c90f283
tomosu20 <tomosu.cre820@gmail.com>
2024-06-06 17:57:53 +0900

    add formrequest

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UploadImageRequest;
 use App\Models\Shop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -47,7 +48,7 @@ class ShopController extends Controller
         return view('owner.shops.edit', compact('shop'));
     }
 
-    public function update(Request $request)
+    public function update(UploadImageRequest $request)
     {
         $imageFile = $request->image;
         if (!is_null($imageFile) && $imageFile->isValid()) {
