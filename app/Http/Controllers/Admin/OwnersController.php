8a95dd104d38d7b5f22087cc1343e2f64d36cd1e
tomosu20 <tomosu.cre820@gmail.com>
2024-06-06 12:41:09 +0900

    edit, update

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -39,7 +39,7 @@ class OwnersController extends Controller
 
         // dd($e_all, $q_get, $q_first, $c_test);
 
-        $owners = Owner::select('name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
 
         return view(
             'admin.owners.index',
@@ -101,7 +101,10 @@ class OwnersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        // dd($owner);
+
+        return view('admin.owners.edit', compact('owner'));
     }
 
     /**
@@ -113,7 +116,15 @@ class OwnersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        $owner->name = $request->name;
+        $owner->email = $request->email;
+        $owner->password = Hash::make($request->password);
+        $owner->save();
+
+        return redirect()
+            ->route('admin.owners.index')
+            ->with('message', 'オーナー情報を更新しました。');
     }
 
     /**
