715ff593373f6a7df42656d362a529143e39a835
tomosu20 <tomosu.cre820@gmail.com>
2024-06-07 15:59:48 +0900

    product store

--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -31,7 +31,7 @@
                             <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative">
                                     <label for="price" class="leading-7 text-sm text-gray-600">価格</label>
-                                    <input type="number" id="price" price="name" value="{{ old('price') }}"
+                                    <input type="number" id="price" name="price" value="{{ old('price') }}"
                                         required
                                         class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                 </div>
