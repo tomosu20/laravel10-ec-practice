983f1505f4e3c5b0a3734f57503d2f05983a0b0c
tomosu20 <tomosu.cre820@gmail.com>
2024-06-07 12:35:31 +0900

    category

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Models\Image;
 use App\Models\Owner;
+use App\Models\PrimaryCategory;
 use App\Models\Product;
+use App\Models\Shop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -45,7 +48,22 @@ class ProductController extends Controller
 
     public function create()
     {
-        //
+        $shops = Shop::where('owner_id', Auth::id())
+            ->select('id', 'name')
+            ->get();
+
+        $images = Image::where('owner_id', Auth::id())
+            ->select('id', 'title', 'filename')
+            ->orderBy('updated_at', 'desc')
+            ->get();
+
+        $categories = PrimaryCategory::with('secondary')
+            ->get();
+
+        return view(
+            'owner.products.create',
+            compact('shops', 'images', 'categories')
+        );
     }
 
     public function store(Request $request)
