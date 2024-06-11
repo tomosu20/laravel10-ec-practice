5bf57fc9550e58ec3bf48ab0e6e58f758492dbc3
tomosu20 <tomosu.cre820@gmail.com>
2024-06-11 01:16:12 +0000

    cartService作成

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Cart;
 use App\Models\Stock;
 use App\Models\User;
+use App\Services\CartService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -52,6 +53,11 @@ class CartController extends Controller
 
     public function checkout()
     {
+        ////
+        $items = Cart::where('user_id', Auth::id())->get();
+        $products = CartService::getItemsInCart($items);
+        ////
+
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
 
