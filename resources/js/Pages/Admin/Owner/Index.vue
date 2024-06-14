b8195562a67e0aea61424ce0bc5b984ec419c7bc
tomosu20 <tomosu.cre820@gmail.com>
2024-06-14 09:15:30 +0000

    add flash message

--- resources/js/Pages/Admin/Owner/Index.vue
+++ resources/js/Pages/Admin/Owner/Index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import DangerButton from '@/Components/DangerButton.vue';
+import FlashMessage from '@/Components/FlashMessage.vue';
 import SecondaryButton from '@/Components/SecondaryButton.vue';
 import AuthenticatedLayout from '@/Layouts/AdminAuthenticatedLayout.vue';
 import { Head, Link, router } from '@inertiajs/vue3';
@@ -29,7 +30,7 @@ const deleteOwner = id => {
                     <div class="md:p-6 bg-white border-b border-gray-200">
                         <section class="text-gray-600 body-font">
                             <div class="container md:px-5 mx-auto">
-                                <!-- <x-flash-message status="session('status')" />　//TODO: フラッシュメッセージ -->
+                                <FlashMessage />
                                 <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                                     <div class="flex justify-end m-4">
                                         <Link :href="route('admin.owners.create')">
