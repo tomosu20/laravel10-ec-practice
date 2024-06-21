d90d35f7e01307b71eed27c849a4c90437dc451a
tomosu20 <tomosu.cre820@gmail.com>
2024-06-21 00:41:14 +0000

    ページを移動しても検索条件を保持させる。 ソートや表示件数を変更すると検索条件は保持されるが1ページ目が表示される

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,6 +1,24 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
-defineProps({ links: Array });
+const props = defineProps({
+    links: Array,
+    query: {
+        type: URLSearchParams,
+        default: new URLSearchParams(window.location.search),
+    },
+});
+
+const queryPath = (page) => {
+    props.query.delete('page')
+    const location = window.location
+    const basePath = location.origin + location.pathname
+    const queryPath = props.query.toString()
+    if (queryPath === '') {
+        return basePath + '?page=' + page
+    }
+    return basePath + '?' + queryPath + '&page=' + page
+}
+
 </script>
 
 <template>
@@ -11,7 +29,8 @@ defineProps({ links: Array });
                     v-html="link.label" />
                 <Link v-else
                     class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-gray-300 focus:border-indigo-500 focus:text-indigo-500"
-                    :class="{ 'bg-blue-700 text-white': link.active }" :href="link.url" v-html="link.label" />
+                    :class="{ 'bg-blue-700 text-white': link.active }" :href="queryPath(link.label)"
+                    v-html="link.label" />
             </template>
         </div>
     </div>
--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -1,12 +1,27 @@
 <script setup>
 import Pagination from '@/Components/Pagination.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
 import Thumbnail from '@/Components/Thumbnail.vue';
 import AuthenticatedLayout from '@/Layouts/UserAuthenticatedLayout.vue';
-import { Head, Link } from '@inertiajs/vue3';
+import { Head, Link, useForm } from '@inertiajs/vue3';
 
-defineProps({
-    products: Object
+const props = defineProps({
+    products: Object,
+    categories: Object
 });
+
+const params = new URLSearchParams(window.location.search);
+
+const form = useForm({
+    category: params.get('category') ?? 0,
+    keyword: params.get('keyword'),
+    sort: params.get('sort') ?? 0,
+    pagination: params.get('pagination') ?? 20,
+})
+
+const getProductList = () => {
+    form.get(route('user.items.index'));
+}
 
 </script>
 
@@ -21,6 +36,53 @@ defineProps({
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="mb-4">
+                    <form @submit.prevent="getProductList">
+                        <div class="lg:flex lg:justify-between">
+                            <div class="lg:flex items-center">
+                                <select class="mb-2 lg:mb-0 lg:mr-2" v-model="form.category">
+                                    <option value="0">全て</option>
+                                    <template v-for="category in categories">
+                                        <optgroup :label="category.name">
+                                            <option v-for="secondary in category.secondary" :value="secondary.id">
+                                                {{ secondary.name }}
+                                            </option>
+                                        </optgroup>
+                                    </template>
+                                </select>
+                                <div class="flex space-x-2 items-center mb-2 lg:mb-0 lg:mr-2">
+                                    <div>
+                                        <input type="text" v-model="form.keyword" placeholder="キーワードを入力">
+                                    </div>
+                                    <div>
+                                        <PrimaryButton type="submit">検索する</PrimaryButton>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex">
+                                <div>
+                                    <span class="text-sm mr-2">表示順</span>
+                                    <select id="sort" v-model="form.sort" class="mr-4" @change="getProductList">
+                                        <option value="0">おすすめ順</option>
+                                        <option value="1">料金の高い順</option>
+                                        <option value="2">料金の安い順</option>
+                                        <option value="3">新しい順</option>
+                                        <option value="4">古い順</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <span class="text-sm mr-2">表示件数</span>
+                                    <select id="pagination" v-model="form.pagination" @change="getProductList">
+                                        <option value="20">20件</option>
+                                        <option value="50">50件</option>
+                                        <option value="100">100件</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
                         <div class="flex flex-wrap">
@@ -42,7 +104,7 @@ defineProps({
                                 </Link>
                             </div>
                         </div>
-                        <Pagination class="mt-6" :links="products.links" />
+                        <Pagination class="mt-6" :links="products.links" :query="params" />
                     </div>
                 </div>
             </div>
